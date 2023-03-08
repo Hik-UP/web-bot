e4e3a1a34723f0133a848be3766b8046f1e85fe6
NeantBetaS <71332802+NeantBetaS@users.noreply.github.com>
2023-03-08 07:40:40 -0500

    optimise website & add social

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { ReactElement, useEffect, useRef, useState } from 'react';
 import { config, Spring, animated, SpringValue } from 'react-spring';
 
 import Button from '@mui/material/Button';
@@ -18,6 +18,12 @@ import TimelineContent from '@mui/lab/TimelineContent';
 import TimelineOppositeContent from '@mui/lab/TimelineOppositeContent';
 import TimelineDot from '@mui/lab/TimelineDot';
 
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
+
 import DownloadIcon from '@mui/icons-material/Download';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import RocketLaunchIcon from '@mui/icons-material/RocketLaunch';
@@ -29,6 +35,8 @@ import StorageIcon from '@mui/icons-material/Storage';
 import BugReportIcon from '@mui/icons-material/BugReport';
 import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
 import NewReleasesIcon from '@mui/icons-material/NewReleases';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import EmailIcon from '@mui/icons-material/Email';
 
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
@@ -43,7 +51,89 @@ function Home() {
     { id: 'home', name: 'Accueil' },
     { id: 'presentation', name: 'Présentation' },
     { id: 'timeline', name: 'Chronologie' },
-    { id: 'team', name: 'Équipe' }
+    { id: 'team', name: 'Équipe' },
+    { id: 'social', name: 'Réseaux' }
+  ];
+  const timelineList: {
+    date: string;
+
+    // eslint-disable-next-line
+    icon: ReactElement<any, any>;
+    iconColor?:
+      | 'success'
+      | 'error'
+      | 'inherit'
+      | 'grey'
+      | 'primary'
+      | 'secondary'
+      | 'info'
+      | 'warning';
+    iconVariant?: 'outlined' | 'filled';
+    title: string;
+    description: string;
+  }[] = [
+    {
+      date: 'Septembre 2021',
+      icon: <RocketLaunchIcon />,
+      title: 'Création',
+      description: "Naissance du projet Hik'UP."
+    },
+    {
+      date: 'Décembre 2021',
+      icon: <RemoveRedEyeIcon />,
+      iconVariant: 'outlined',
+      title: 'Prototypage',
+      description: 'Vérification de la viabilité du projet.'
+    },
+    {
+      date: 'Janvier 2022',
+      icon: <CheckCircleIcon />,
+      iconColor: 'success',
+      title: 'Validation',
+      description: 'Projet validé par notre équipe pédagogique.'
+    },
+    {
+      date: 'Mars 2022',
+      icon: <CodeIcon />,
+      title: 'Développement',
+      iconVariant: 'outlined',
+      description: 'Début du développement de notre application.'
+    },
+    {
+      date: 'Septembre 2022',
+      icon: <LoopIcon />,
+      iconVariant: 'outlined',
+      title: 'Restructuration',
+      description: "Passage du moteur de rendu d'Unity à Flutter."
+    },
+    {
+      date: 'Janvier 2023',
+      icon: <StorageIcon />,
+      iconVariant: 'outlined',
+      title: 'Backend 2.0',
+      description: 'Réécriture du backend en prévision de notre bêta.'
+    },
+    {
+      date: 'Mai 2023',
+      icon: <BugReportIcon />,
+      iconColor: 'secondary',
+      title: 'Bêta',
+      description: 'Bêta, axée sur son aspect informatif.'
+    },
+    {
+      date: 'Septembre 2023',
+      icon: <SportsEsportsIcon />,
+      iconVariant: 'outlined',
+      title: 'Ludification',
+      description: "Lancement de l'aspect ludique de notre application."
+    },
+    {
+      date: 'Novembre 2023',
+      icon: <NewReleasesIcon />,
+      iconColor: 'primary',
+      title: 'Publication',
+      description: 'Version grand public de notre application.'
+    }
   ];
   const membersList = [
     {
@@ -87,6 +177,20 @@ function Home() {
       img: 'team_7.png',
       name: 'Alexandre BERTHOMIER',
       status: 'Développeur Backend'
+    }
+  ];
+  const socialList = [
+    {
+      icon: <GitHubIcon />,
+      title: 'GitHub',
+      description: 'Suivez notre avancement sur GitHub !',
+      url: 'https://github.com/Hik-UP'
+    },
+    {
+      icon: <EmailIcon />,
+      title: 'Email',
+      description: 'Contactez-nous par mail !',
+      url: 'mailto:hikupapp@gmail.com'
     }
   ];
 
@@ -161,6 +265,12 @@ function Home() {
                     className="nav-dot"
                     style={{ transform }}
                     onClick={() => {
+                      document.body.removeEventListener(
+                        'wheel',
+                        MouseWheelHandler,
+                        false
+                      );
+                      tooltip.current = true;
                       setIndex(i);
                     }}
                   ></animated.button>
@@ -202,12 +312,7 @@ function Home() {
       </div>
       <div className="presentation" id="presentation">
         <div className="title">Présentation</div>
-        <Grid
-          container
-          rowSpacing={1}
-          columnSpacing={{ xs: 1, sm: 2, md: 3 }}
-          className="grid"
-        >
+        <Grid container rowSpacing={5} className="grid">
           <Grid item xs={6} className="grid-item">
             <div className="text">
               Hik'Up est un jeu mobile qui rend la randonnée plus ludique tout
@@ -220,7 +325,6 @@ function Home() {
           <Grid item xs={6} className="grid-item">
             <img
               style={{
-                margin: '3em',
                 height: '10em',
                 width: '10em',
                 borderRadius: '10px'
@@ -232,7 +336,6 @@ function Home() {
           <Grid item xs={6} className="grid-item">
             <img
               style={{
-                margin: '3em',
                 height: '20em',
                 width: '10em',
                 borderRadius: '10px'
@@ -260,224 +363,38 @@ function Home() {
         <div className="title">Chronologie</div>
         <div className="container" id="timeline-container">
           <Timeline position="alternate">
-            <TimelineItem>
-              <TimelineOppositeContent
-                sx={{ m: 'auto 0' }}
-                align="right"
-                variant="body2"
-                color="text.secondary"
-              >
-                Septembre 2021
-              </TimelineOppositeContent>
-              <TimelineSeparator>
-                <TimelineConnector />
-                <TimelineDot>
-                  <RocketLaunchIcon />
-                </TimelineDot>
-                <TimelineConnector />
-              </TimelineSeparator>
-              <TimelineContent sx={{ py: '12px', px: 2 }}>
-                <Typography variant="h6" component="span">
-                  Création
-                </Typography>
-                <Typography>Naissance du projet Hik&apos;UP.</Typography>
-              </TimelineContent>
-            </TimelineItem>
-            <TimelineItem>
-              <TimelineOppositeContent
-                sx={{ m: 'auto 0' }}
-                variant="body2"
-                color="text.secondary"
-              >
-                Décembre 2021
-              </TimelineOppositeContent>
-              <TimelineSeparator>
-                <TimelineConnector />
-                <TimelineDot variant="outlined">
-                  <RemoveRedEyeIcon />
-                </TimelineDot>
-                <TimelineConnector />
-              </TimelineSeparator>
-              <TimelineContent sx={{ py: '12px', px: 2 }}>
-                <Typography variant="h6" component="span">
-                  Prototypage
-                </Typography>
-                <Typography>Vérification de la viabilité du projet.</Typography>
-              </TimelineContent>
-            </TimelineItem>
-            <TimelineItem>
-              <TimelineOppositeContent
-                sx={{ m: 'auto 0' }}
-                variant="body2"
-                color="text.secondary"
-              >
-                Janvier 2022
-              </TimelineOppositeContent>
-              <TimelineSeparator>
-                <TimelineConnector />
-                <TimelineDot color="success">
-                  <CheckCircleIcon />
-                </TimelineDot>
-                <TimelineConnector />
-              </TimelineSeparator>
-              <TimelineContent sx={{ py: '12px', px: 2 }}>
-                <Typography variant="h6" component="span">
-                  Validation
-                </Typography>
-                <Typography>
-                  Projet validé par notre équipe pédagogique.
-                </Typography>
-              </TimelineContent>
-            </TimelineItem>
-            <TimelineItem>
-              <TimelineOppositeContent
-                sx={{ m: 'auto 0' }}
-                variant="body2"
-                color="text.secondary"
-              >
-                Mars 2022
-              </TimelineOppositeContent>
-              <TimelineSeparator>
-                <TimelineConnector />
-                <TimelineDot>
-                  <CodeIcon />
-                </TimelineDot>
-                <TimelineConnector />
-              </TimelineSeparator>
-              <TimelineContent sx={{ py: '12px', px: 2 }}>
-                <Typography variant="h6" component="span">
-                  Développement
-                </Typography>
-                <Typography>
-                  Début du développement de notre application.
-                </Typography>
-              </TimelineContent>
-            </TimelineItem>
-            <TimelineItem>
-              <TimelineOppositeContent
-                sx={{ m: 'auto 0' }}
-                variant="body2"
-                color="text.secondary"
-              >
-                Septembre 2022
-              </TimelineOppositeContent>
-              <TimelineSeparator>
-                <TimelineConnector />
-                <TimelineDot variant="outlined">
-                  <LoopIcon />
-                </TimelineDot>
-                <TimelineConnector />
-              </TimelineSeparator>
-              <TimelineContent sx={{ py: '12px', px: 2 }}>
-                <Typography variant="h6" component="span">
-                  Restructuration
-                </Typography>
-                <Typography>
-                  Passage du moteur de rendu de notre carte d'Unity à Flutter.
-                </Typography>
-              </TimelineContent>
-            </TimelineItem>
-            <TimelineItem>
-              <TimelineOppositeContent
-                sx={{ m: 'auto 0' }}
-                variant="body2"
-                color="text.secondary"
-              >
-                Janvier 2023
-              </TimelineOppositeContent>
-              <TimelineSeparator>
-                <TimelineConnector />
-                <TimelineDot variant="outlined">
-                  <StorageIcon />
-                </TimelineDot>
-                <TimelineConnector />
-              </TimelineSeparator>
-              <TimelineContent sx={{ py: '12px', px: 2 }}>
-                <Typography variant="h6" component="span">
-                  Backend 2.0
-                </Typography>
-                <Typography>
-                  Réécriture du backend en prévision du lancement de notre bêta.
-                </Typography>
-              </TimelineContent>
-            </TimelineItem>
-            <TimelineItem>
-              <TimelineOppositeContent
-                sx={{ m: 'auto 0' }}
-                variant="body2"
-                color="text.secondary"
-              >
-                Mai 2023
-              </TimelineOppositeContent>
-              <TimelineSeparator>
-                <TimelineConnector />
-                <TimelineDot color="secondary">
-                  <BugReportIcon />
-                </TimelineDot>
-                <TimelineConnector />
-              </TimelineSeparator>
-              <TimelineContent sx={{ py: '12px', px: 2 }}>
-                <Typography variant="h6" component="span">
-                  Bêta
-                </Typography>
-                <Typography>
-                  Bêta de notre application, axée sur son aspect informatif.
-                </Typography>
-              </TimelineContent>
-            </TimelineItem>
-            <TimelineItem>
-              <TimelineOppositeContent
-                sx={{ m: 'auto 0' }}
-                variant="body2"
-                color="text.secondary"
-              >
-                Septembre 2023
-              </TimelineOppositeContent>
-              <TimelineSeparator>
-                <TimelineConnector />
-                <TimelineDot variant="outlined">
-                  <SportsEsportsIcon />
-                </TimelineDot>
-                <TimelineConnector />
-              </TimelineSeparator>
-              <TimelineContent sx={{ py: '12px', px: 2 }}>
-                <Typography variant="h6" component="span">
-                  Ludification
-                </Typography>
-                <Typography>
-                  Lancement de l'aspect ludique de notre application.
-                </Typography>
-              </TimelineContent>
-            </TimelineItem>
-            <TimelineItem>
-              <TimelineOppositeContent
-                sx={{ m: 'auto 0' }}
-                variant="body2"
-                color="text.secondary"
-              >
-                Novembre 2023
-              </TimelineOppositeContent>
-              <TimelineSeparator>
-                <TimelineConnector />
-                <TimelineDot color="primary">
-                  <NewReleasesIcon />
-                </TimelineDot>
-                <TimelineConnector />
-              </TimelineSeparator>
-              <TimelineContent sx={{ py: '12px', px: 2 }}>
-                <Typography variant="h6" component="span">
-                  Publication
-                </Typography>
-                <Typography>
-                  Lancement de la version grand public de notre application.
-                </Typography>
-              </TimelineContent>
-            </TimelineItem>
+            {timelineList.map((item) => (
+              <TimelineItem>
+                <TimelineOppositeContent
+                  sx={{ m: 'auto 0' }}
+                  variant="body2"
+                  color="text.secondary"
+                >
+                  {item.date}
+                </TimelineOppositeContent>
+                <TimelineSeparator>
+                  <TimelineConnector />
+                  <TimelineDot
+                    color={item.iconColor}
+                    variant={item.iconVariant}
+                  >
+                    {item.icon}
+                  </TimelineDot>
+                  <TimelineConnector />
+                </TimelineSeparator>
+                <TimelineContent sx={{ py: '12px', px: 2 }}>
+                  <Typography variant="h6" component="span">
+                    {item.title}
+                  </Typography>
+                  <Typography>{item.description}</Typography>
+                </TimelineContent>
+              </TimelineItem>
+            ))}
           </Timeline>
         </div>
       </div>
       <div className="team" id="team">
-        <div className="title">Notre équipe</div>
+        <div className="title">Équipe</div>
         <ImageList cols={4} className="container">
           {membersList.map((item) => (
             <ImageListItem key={item.key}>
@@ -495,6 +412,23 @@ function Home() {
             </ImageListItem>
           ))}
         </ImageList>
+      </div>
+      <div className="social" id="social">
+        <div className="title">Réseaux</div>
+        <List sx={{ width: '100%', maxWidth: 360 }} className="list">
+          {socialList.map((item) => (
+            <ListItem className="list-item">
+              <ListItemAvatar>
+                <Avatar>{item.icon}</Avatar>
+              </ListItemAvatar>
+              <ListItemText
+                primary={item.title}
+                secondary={item.description}
+                onClick={() => window.open(item.url, '_blank')}
+              />
+            </ListItem>
+          ))}
+        </List>
       </div>
     </div>
   );
