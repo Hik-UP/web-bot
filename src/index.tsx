001c25013334bcdeef857897d1d2b599e36f1dec
NeantBetaS <71332802+NeantBetaS@users.noreply.github.com>
2023-03-08 09:34:19 -0500

    fix slider bug

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter /*basename={process.env.PUBLIC_URL}*/>
       <App />
     </BrowserRouter>
   </React.StrictMode>
